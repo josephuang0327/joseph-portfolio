4c8706840ee5ba429e7d29a689e3e39831c6a76c
josephhuang <josephuang0327@gmail.com>
2024-03-15 18:44:56 +0800

    setup basic import

---
+++ src/components/Navbar.js
@@ -0,0 +1,9 @@
+import { useState } from "react";
+
+function Navbar(){
+    return (
+        <>
+            div.nav-bar
+        </>
+    )
+}
---
+++ src/pages/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Navbar from '../components/Navbar';
+import Footer from '../components/Footer';
+function Home(){
+    return (
+        <>
+            <Navbar/>
+            <Footer/>
+        </>
+    )
+}
